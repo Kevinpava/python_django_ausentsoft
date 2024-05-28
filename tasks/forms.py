f1b4b31cfb6e79359bc39d93959c5f80acfc45f0
Jasnier <jasnier41@gmail.com>
2024-05-28 17:29:41 -0500

    Cambios de vista en los formularios singin y singup
    :)

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,6 +1,24 @@
 from django.forms import ModelForm
 from .models import Task
 
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+
+
+
+
 class TaskForm (ModelForm):
     class Meta:
         model = Task
--- tasks/views.py
+++ tasks/views.py
@@ -6,12 +6,35 @@ from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
 from django.utils import timezone
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': CustomUserCreationForm()
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': CustomUserCreationForm(),
+                    "error": 'este usuario ya existe'
+                })
+        return render(request, 'signup.html', {
+            'form': CustomUserCreationForm(),
+            "error": 'contraseña no coincide'
+        })
 
     if request.method == 'GET':
         return render(request, 'signup.html',{
